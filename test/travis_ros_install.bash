a3c7f540e33c4305781d8d75df1e54cea430c7b0
Tomohiko Nomura <tomohiko.nomura@sound.ocn.ne.jp>
2018-10-14 10:49:25 +0000

    Change a bash script file

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,22 +1,23 @@
 #!/bin/bash -xve
 
 #required packages
-pip install pip install --upgrade catkin_pkg
-pip install empy
-pip install pyyaml
-pip install rospkg
+pip install ---upgrade pip==18.1
+#pip install catkin_pkg
+#pip install empy
+#pip install pyyaml
+#pip install rospkg
 
 #ros install
-cd ..
-git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
-cd ./ros_setup_scripts_Ubuntu14.04_server
-bash ./setup0.bash
-bash ./setup1.bash
+#cd ..
+#git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
+#cd ./ros_setup_scripts_Ubuntu14.04_server
+#bash ./setup0.bash
+#bash ./setup1.bash
 
 #catkin setup
-mkdir -p ~/catkin_ws/src
-source /opt/ros/indigo/setup.bash
-catkin_init_workspace
-cd ~/catkin_ws
-catkin_amke
+#mkdir -p ~/catkin_ws/src
+#source /opt/ros/indigo/setup.bash
+#catkin_init_workspace
+#cd ~/catkin_ws
+#catkin_amke
 
