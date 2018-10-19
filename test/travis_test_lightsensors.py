3967dde839f1fa92dd6a98061398457c473a23b2
Tomohiko Nomura <tomohiko.nomura@sound.ocn.ne.jp>
2018-10-19 14:46:11 +0000

    Add LightSensors module.

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -1,17 +1,18 @@
-#!/usr/bin env python
+#!/usr/bin/env python
 #encoding: utf8
 
 import unittest, rostest
 import rosnode, rospy
 import time
-from pimouse_ros,msg import LightSensorValues
+from pimouse_ros.msg import LightSensorValues
 
 class LightsensorTest(unittest.TestCase):
     def setUp(self):
         self.count = 0
         rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
+        self.values = LightSensorValues()
                                     
-    def callback(self.data):
+    def callback(self,data):
         self.count += 1
         self.values = data
 
@@ -27,23 +28,26 @@ class LightsensorTest(unittest.TestCase):
 
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
-        self.assertIn('lightsensors_freq',10)
-        time.sleep(2)
-        with open("/dev/rtlightsensor0","w") as f:
+        self.assertIn('/lightsensors', nodes, "node does not exist")
+
+    def test_get_value(self):
+        rospy.set_param('lightsensors_freq',10)    #センサの値取得の周期を10Hzに
+        time.sleep(2)                              #パラメータの反映を待つ
+        with open("/dev/rtlightsensor0","w") as f: #ダミーの値をダミーのファイルに
             f.write("-1 0 123 4321\n")
 
         time.sleep(3)
-        ###コールバック関数が最低1回は呼ばれ、阿合が取得できているかを確認###
+        ###コールバック関数が最低1回は呼ばれ、値が取得できているかを確認###
         self.assertFalse(self.count == 0, "cannot subscribe the topic")
         self.check_values(4321,123,0,-1)
 
     def test_change_parameter(self):
-        rospy.set_param('lightsensors_freq',1)
-        time.sleep(2)
-        c_prev = self.count
+        rospy.set_param('lightsensors_freq',1)  #センサの値取得の周期を1Hzに
+        time.sleep(2)                           #反映を待つ
+        c_prev = self.count                     #callbackが呼ばれた回数を記録
         time.sleep(3)
                                                                                                                                                                                                                                                             
-        ####コールバック関数が3秒間で最低1回、最高でも4回しか呼ばれないことを確認
+        ###コールバック関数が3秒間で最低1回、最高でも4回しか呼ばれてないことを確認###
         self.assertTrue(self.count < c_prev + 4 , "freq does not change")
         self.assertFalse(self.count == c_prev   , "subscriber is stopped")
 
