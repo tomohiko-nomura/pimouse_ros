3c206e56c5e2fb802ba489a15ed82d12c89e47db
Tomohiko Nomura <tomohiko.nomura@sound.ocn.ne.jp>
2018-10-19 22:35:25 +0000

    Add LightSensors module.

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -54,5 +54,5 @@ class LightsensorTest(unittest.TestCase):
 if __name__ == '__main__':
     time.sleep(3)
     rospy.init_node('travis_test_lightsensors')
-    rostest.rosrun('pimouse_ros', 'travis_test_lightsensors', LightsensorTest)
+    rostest.rosrun('pimouse_ros','travis_test_lightsensors',LightsensorTest)
 
