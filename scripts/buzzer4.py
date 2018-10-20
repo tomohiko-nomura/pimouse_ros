aeac2650e82bea0af17e1598f6fa11e2ccb9cc93
Tomohiko Nomura <tomohiko.nomura@sound.ocn.ne.jp>
2018-10-20 12:33:15 +0000

    Add Final Buzzer Module.

--- scripts/buzzer4.py
+++ scripts/buzzer4.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-import rospy
+#encoding: utf8
+import rospy, actionlib
 from std_msgs.msg import UInt16
+from pimouse_ros.msg import MusicAction, MusicResult, MusicFeedback # 行を追加
 
 def write_freq(hz=0):
     bfile = "/dev/rtbuzzer0"
@@ -10,11 +12,15 @@ def write_freq(hz=0):
     except IOError:
         rospy.logerr("can't write to " + bfile)
 
+def exec_music(goal): pass        # 追加
+
 def recv_buzzer(data):
     write_freq(data.data)
-
 if __name__ == '__main__':
     rospy.init_node('buzzer')
     rospy.Subscriber("buzzer", UInt16, recv_buzzer)
+    music = actionlib.SimpleActionServer('music', MusicAction, exec_music, False) # 追加
+    music.start()                                                                 # 追加
+    rospy.on_shutdown(write_freq)                                                 # 追加
     rospy.spin()
 
